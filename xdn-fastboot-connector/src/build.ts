bcf09ced1c0da2bcbd2223214bc492c2c8e90526
GitHub Action <action@github.com>
2021-03-31 12:24:17 +0000

    Connectors for version v2.49.0

--- xdn-fastboot-connector/src/build.ts
+++ xdn-fastboot-connector/src/build.ts
@@ -16,21 +16,21 @@ module.exports = async function build(options: BuildOptions) {
 
   if (!options.skipFramework) {
     try {
-      await builder.exec('npx frontity build')
+      await builder.exec('npx ember build --environment=production')
     } catch (e) {
-      throw new FrameworkBuildError('Frontity')
+      throw new FrameworkBuildError('FastBoot')
     }
   }
 
   if (existsSync(SW_SOURCE)) {
     console.log('> Building service worker...')
     await buildServiceWorker()
-    unlinkSync(resolve(process.cwd(), 'build', '__delete_me__.js'))
+    unlinkSync(resolve(process.cwd(), 'dist', '__delete_me__.js'))
   } else {
     console.warn('> sw/service-worker.js not found... skipping.')
   }
 
-  builder.addJSAsset('build')
+  builder.addJSAsset('dist')
 
   await builder.build()
 }
@@ -51,10 +51,10 @@ function buildServiceWorker() {
 
         if (stats) {
           const { warnings, errors } = stats.toJson()
-          warnings.forEach((item: any) => console.warn(item.message || item))
-          errors.forEach((item: any) => console.error(item.message || item))
+          warnings?.forEach((item: any) => console.warn(item.message || item))
+          errors?.forEach((item: any) => console.error(item.message || item))
 
-          if (errors.length) {
+          if (errors?.length) {
             console.error(errors)
             return reject(new Error('Build failed.'))
           } else {
